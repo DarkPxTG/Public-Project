daa1dd2fe572f36d3d936102493e9352c4968451
DarkPxTG <topo00861@gmail.com>
2025-02-14 14:21:25 +0330

    CheckFootprint.tsx

--- components/CheckFootprint.tsx
+++ components/CheckFootprint.tsx
@@ -5,9 +5,9 @@ import ArrowBigRight from "@/icons/ArrowBigRight"
 const CheckFootprint = () => {
     return (
         <div className="flex justify-center w-full">
-            <div className="fixed top-0 w-full max-w-md px-4 py-3 bg-[#202020] cursor-pointer rounded-full z-20">
-                <div className="flex justify-between items-center pl-2 border-l-[2px] border-[#39a3ff] rounded-full">
-                    <div className="text-base text-white font-medium">Check the Dark Chanel</div>
+            <div className="fixed top-0 w-full max-w-md px-4 py-3 bg-[#333333] cursor-pointer z-20">
+                <div className="flex justify-between items-center pl-2 border-l-[2px] border-[#39a3ff]">
+                    <div className="text-base text-white font-medium">Check the NATOI Chanel</div>
                     {/* لینک دادن به فلش آبی */}
                     <a href="https://t.me/Rabbit_coinR" target="_blank" rel="noopener noreferrer">
                         <button className="bg-[#319af7] rounded-full px-2 py-1">
